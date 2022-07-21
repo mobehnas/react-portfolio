14fdda9b0e08e26aa50581056076d038fb1b33de
mohamed behnas <behna@DESKTOP-T1K4JBI.(none)>
2022-07-21 15:47:33 -0400

    resizing images

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -6,7 +6,8 @@ function About() {
     <section class="about">
       <h1 id="about">Hi, My Name is Mohamed Behnas</h1>
       <h2>And this is My portfolio</h2>
-      <p>I am a Case Reserve Western University full stack web developer from Cleveland,Ohio </p>
+      <p >“I am ambitious and driven. I thrive on challenge and constantly set goals for myself, so I have something to strive toward. I’m not comfortable with settling, and I’m always looking for an opportunity to do better and achieve greatness” 
+      </p>
     </section>
   );
 }
