0efe954a9c3e580c361442a1dd4aa36041471b35
mohamed behnas <behna@DESKTOP-T1K4JBI.(none)>
2022-07-23 18:59:18 -0400

    change class to className

--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -9,17 +9,17 @@ import budgetTrackerImage from "../../assets/images/budget-tracker.png";
 
 function Portfolio() {
   return (
-    <section class="portfolio">
+    <section className="portfolio">
 
-      <div class="row row-cols-1 row-cols-sm-2 row-cols-md-2 row-cols-lg-3 g-4">
-        <div class="col">
-          <div class="card">
+      <div className="row row-cols-1 row-cols-sm-2 row-cols-md-2 row-cols-lg-3 g-4">
+        <div className="col">
+          <div className="card">
             <img
               src={foodFestivalImage}
               alt="food festival"
-              class="card-img-top"
+              className="card-img-top"
             />
-            <div class="card-body">
+            <div className="card-body">
 
               <a href="https://github.com/nbross/food-drink-api-project" target="_blank" rel="noreferrer">
                 <button>GitHub</button>
@@ -32,14 +32,14 @@ function Portfolio() {
         </div>
 
 
-        <div class="col">
-          <div class="card">
+        <div className="col">
+          <div className="card">
             <img
               src={passwordGeneratorImage}
               alt="password Generator"
-              class="card-img-top"
+              className="card-img-top"
             />
-            <div class="card-body">
+            <div className="card-body">
               <a href="https://github.com/mobehnas/password-generator" target="_blank" rel="noreferrer">
                 <button>GitHub</button>
               </a>
@@ -50,14 +50,14 @@ function Portfolio() {
           </div>
         </div>
 
-        <div class="col">
-          <div class="card">
+        <div className="col">
+          <div className="card">
             <img
               src={invoiceTrackerImage}
               alt="invoice Tracker"
-              class="card-img-top"
+              className="card-img-top"
             />
-            <div class="card-body">
+            <div className="card-body">
               <a href="https://github.com/zachburkhart/invoice-tracker" target="_blank" rel="noreferrer">
                 <button>GitHub</button>
               </a>
@@ -69,14 +69,14 @@ function Portfolio() {
         </div>
 
 
-        <div class="col">
-          <div class="card">
+        <div className="col">
+          <div className="card">
             <img
               src={weatherDashboardImage}
               alt="weather Dashboar"
-              class="card-img-top"
+              className="card-img-top"
             />
-            <div class="card-body">
+            <div className="card-body">
               <a href="https://github.com/mobehnas/Weather-Dashboard" target="_blank" rel="noreferrer">
                 <button>GitHub</button>
               </a>
@@ -87,14 +87,14 @@ function Portfolio() {
           </div>
         </div>
 
-        <div class="col">
-          <div class="card">
+        <div className="col">
+          <div className="card">
             <img
               src={techBlogImage}
               alt="tech Blog"
-              class="card-img-top"
+              className="card-img-top"
             />
-            <div class="card-body">
+            <div className="card-body">
               <a href="https://github.com/mobehnas/mvc-tech-blog" target="_blank" rel="noreferrer">
                 <button>GitHub</button>
               </a>
@@ -105,14 +105,14 @@ function Portfolio() {
           </div>
         </div>
 
-        <div class="col">
-          <div class="card">
+        <div className="col">
+          <div className="card">
             <img
               src={budgetTrackerImage}
               alt="budget Tracker"
-              class="card-img-top"
+              className="card-img-top"
             />
-            <div class="card-body">
+            <div className="card-body">
               <a href="https://github.com/mobehnas/Progressive-Web-Applications-PWA--Budget-Tracker" target="_blank" rel="noreferrer">
                 <button>GitHub</button>
               </a>
